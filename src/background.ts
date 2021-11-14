bee72df6bef7c4cdb9971c9b7970ba5aab8422d0
TidalSurf <64665142+TidalSurf@users.noreply.github.com>
2021-11-13 22:24:18 -0600

    fix error

--- src/background.ts
+++ src/background.ts
@@ -6,59 +6,61 @@ const { detect } = require("detect-browser");
 const browser = detect();
 const config = require("../config.json");
 const provider = createMetaMaskProvider();
-import { ethers } from "ethers";
+const Eth = require("ethjs");
 import { OpenSeaPort, Network } from "opensea-js";
 import { OrderSide } from "opensea-js/lib/types";
+const customHttpProvider = new Eth(provider);
+var accountAddress: any;
 
-var customHttpProvider = new ethers.providers.Web3Provider(provider);
-const seaport = new OpenSeaPort(customHttpProvider, {
-	networkName: Network.Main,
+customHttpProvider.accounts().then((accounts: any) => {
+  accountAddress = accounts[0];
+  console.log(`Detected MetaMask account ${accounts[0]}`);
 });
-customHttpProvider.send("eth_requestAccounts", []);
 
-var signer = customHttpProvider.getSigner();
-var account = signer.getAddress();
+const seaport = new OpenSeaPort(provider, {
+  networkName: Network.Main,
+});
 chrome.runtime.onMessage.addListener(async function (
-	request: any,
-	sender: any
+  request: any,
+  sender: any
 ) {
-	if (request?.params) {
-		console.log(request?.params);
-		const order = await seaport.api.getOrder({
-			side: OrderSide.Sell,
-			asset_contract_address: request?.params?.assetId,
-			token_id: request?.params?.tokenId,
-		});
-		const accountAddress = await account;
-		const transactionHash = await seaport.fulfillOrder({
-			order,
-			accountAddress,
-		});
-		console.log(transactionHash);
-	}
+  if (request?.params) {
+    console.log(request?.params);
+    const order = await seaport.api.getOrder({
+      side: OrderSide.Sell,
+      asset_contract_address: request?.params?.assetId,
+      token_id: request?.params?.tokenId,
+    });
+
+    const transactionHash = await seaport.fulfillOrder({
+      order,
+      accountAddress,
+    });
+    console.log(transactionHash);
+  }
 });
 
 function createMetaMaskProvider() {
-	let provider;
-	try {
-		let currentMetaMaskId = getMetaMaskId();
-		const metamaskPort = chrome.runtime.connect(currentMetaMaskId);
-		const pluginStream = new PortStream(metamaskPort);
-		provider = new MetaMaskInpageProvider(pluginStream);
-	} catch (e) {
-		console.dir(`Metamask connect error `, e);
-		throw e;
-	}
-	return provider;
+  let provider;
+  try {
+    let currentMetaMaskId = getMetaMaskId();
+    const metamaskPort = chrome.runtime.connect(currentMetaMaskId);
+    const pluginStream = new PortStream(metamaskPort);
+    provider = new MetaMaskInpageProvider(pluginStream);
+  } catch (e) {
+    console.dir(`Metamask connect error `, e);
+    throw e;
+  }
+  return provider;
 }
 
 function getMetaMaskId() {
-	switch (browser && browser.name) {
-		case "chrome":
-			return config.CHROME_ID;
-		case "firefox":
-			return config.FIREFOX_ID;
-		default:
-			return config.CHROME_ID;
-	}
+  switch (browser && browser.name) {
+    case "chrome":
+      return config.CHROME_ID;
+    case "firefox":
+      return config.FIREFOX_ID;
+    default:
+      return config.CHROME_ID;
+  }
 }
