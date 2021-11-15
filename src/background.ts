a415d01175adbeb71bc874d463b216b395989960
TidalSurf <64665142+TidalSurf@users.noreply.github.com>
2021-11-15 15:11:39 -0600

    error handling and minor refactor

--- src/background.ts
+++ src/background.ts
@@ -5,61 +5,101 @@ const PortStream = require("extension-port-stream");
 const { detect } = require("detect-browser");
 const browser = detect();
 const config = require("../config.json");
-const provider = createMetaMaskProvider();
+const channel = new BroadcastChannel("Funimation");
 import { ethers } from "ethers";
 import { OpenSeaPort, Network } from "opensea-js";
 import { OrderSide } from "opensea-js/lib/types";
 
-var customHttpProvider = new ethers.providers.Web3Provider(provider);
+var customHttpProvider: any;
 var account: string;
+var seaport: any;
+var error: object;
+var success: object;
 
-customHttpProvider.send("eth_requestAccounts", []).then((res: any) => {
-	account = res[0];
-});
-const seaport = new OpenSeaPort(provider, {
-	networkName: Network.Main,
-});
 chrome.runtime.onMessage.addListener(async function (
-	request: any,
-	sender: any
+  request: any,
+  sender: any
 ) {
-	if (request?.params) {
-		console.log(request?.params);
-		const order = await seaport.api.getOrder({
-			side: OrderSide.Sell,
-			asset_contract_address: request?.params?.assetId,
-			token_id: request?.params?.tokenId,
-		});
-		const accountAddress = account;
-		const transactionHash = await seaport.fulfillOrder({
-			order,
-			accountAddress,
-		});
-		console.log(transactionHash);
-	}
+  if (request?.params) {
+    console.log(request?.params);
+    const order = await seaport.api.getOrder({
+      side: OrderSide.Sell,
+      asset_contract_address: request?.params?.assetId,
+      token_id: request?.params?.tokenId,
+    });
+    const accountAddress = account;
+    await seaport
+      .fulfillOrder({
+        order,
+        accountAddress,
+      })
+      .then((res: any) => {
+        console.log(res);
+        success = {
+          success: true,
+          message: `Order is processing: Txhash - ${res}`,
+        };
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs: any) {
+            chrome.tabs.sendMessage(tabs[0].id, { success });
+          }
+        );
+      })
+      .catch((err: any) => {
+        console.log(err?.message);
+
+        if (err?.message.includes("insufficient funds")) {
+          error = {
+            error: false,
+            message: "Insufficient balance for this transaction",
+          };
+          chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs: any) {
+              chrome.tabs.sendMessage(tabs[0].id, { error });
+            }
+          );
+        }
+      });
+  }
 });
 
-function createMetaMaskProvider() {
-	let provider;
-	try {
-		let currentMetaMaskId = getMetaMaskId();
-		const metamaskPort = chrome.runtime.connect(currentMetaMaskId);
-		const pluginStream = new PortStream(metamaskPort);
-		provider = new MetaMaskInpageProvider(pluginStream);
-	} catch (e) {
-		console.dir(`Metamask connect error `, e);
-		throw e;
-	}
-	return provider;
+channel.onmessage = async (msg: any) => {
+  console.log(msg?.data);
+  if (msg?.data) {
+    const provider = createMetaMaskProvider(msg?.data);
+    customHttpProvider = new ethers.providers.Web3Provider(provider);
+    seaport = new OpenSeaPort(provider, {
+      networkName: Network.Main,
+    });
+    customHttpProvider.send("eth_requestAccounts", []).then((res: any) => {
+      account = res[0];
+    });
+  }
+};
+
+function createMetaMaskProvider(mmid?: any) {
+  let provider;
+  try {
+    let currentMetaMaskId = mmid;
+    const metamaskPort = chrome.runtime.connect(currentMetaMaskId);
+    const pluginStream = new PortStream(metamaskPort);
+    provider = new MetaMaskInpageProvider(pluginStream);
+  } catch (e) {
+    console.dir(`Metamask connect error `, e);
+    throw e;
+  }
+  return provider;
 }
 
 function getMetaMaskId() {
-	switch (browser && browser.name) {
-		case "chrome":
-			return config.CHROME_ID;
-		case "firefox":
-			return config.FIREFOX_ID;
-		default:
-			return config.CHROME_ID;
-	}
+  switch (browser && browser.name) {
+    case "chrome":
+      return config.CHROME_ID;
+    case "firefox":
+      return config.FIREFOX_ID;
+    default:
+      return config.CHROME_ID;
+  }
 }
--- src/content.ts
+++ src/content.ts
@@ -6,17 +6,10 @@ import "./content.css";
 const event: EventEmitter = new EventEmitter();
 import utils from "./utils/utils";
 
-chrome.runtime.onMessage.addListener(async function (
-	request: any,
-	sender: any
-) {
-	console.log(request);
-});
-
 console.log("Hello from content script!");
 
 var dom_observer = new MutationObserver(async function (mutation) {
-	await main();
+  await main();
 });
 var container = document.documentElement || document.body;
 var config = { attributes: true, childList: true, characterData: true };
@@ -27,144 +20,166 @@ dom_observer.observe(container, config);
 let tokenId: string;
 let assetId: string;
 async function main() {
-	setInterval(async () => {
-		if (
-			document.location.href.includes("https://opensea.io/activity") ||
-			document.location.href.includes("https://opensea.io/collection")
-		) {
-			let content: any = document.querySelectorAll('[role="listitem"]');
-			if (content) {
-				content.forEach(async function (element: any, idx: number) {
-					if (
-						element.children[0].getElementsByClassName("purchase-button")
-							.length === 0
-					) {
-						const button: HTMLButtonElement = document.createElement("button");
-						button.className = "purchase-button";
-						button.innerHTML = "Cop Now";
+  setInterval(async () => {
+    if (
+      document.location.href.includes("https://opensea.io/activity") ||
+      document.location.href.includes("https://opensea.io/collection")
+    ) {
+      let content: any = document.querySelectorAll('[role="listitem"]');
+      if (content) {
+        content.forEach(async function (element: any, idx: number) {
+          if (
+            element.children[0].getElementsByClassName("purchase-button")
+              .length === 0
+          ) {
+            const button: HTMLButtonElement = document.createElement("button");
+            button.className = "purchase-button";
+            button.innerHTML = "Cop Now";
 
-						button.onclick = async () => {
-							assetId =
-								element.children[0].children[1].children[1].children[0].children[0]
-									.getAttribute("href")
-									.split("/")[2];
-							tokenId =
-								element.children[0].children[1].children[1].children[0].children[0]
-									.getAttribute("href")
-									.split("/")[3];
+            button.onclick = async () => {
+              assetId =
+                element.children[0].children[1].children[1].children[0].children[0]
+                  .getAttribute("href")
+                  .split("/")[2];
+              tokenId =
+                element.children[0].children[1].children[1].children[0].children[0]
+                  .getAttribute("href")
+                  .split("/")[3];
 
-							return event.emit("clickedActivity");
-						};
-						element.children[0].prepend(button);
-					}
-				});
-			}
-		}
+              return event.emit("clickedActivity");
+            };
+            element.children[0].prepend(button);
+          }
+        });
+      }
+    }
 
-		if (document.location.href.includes("https://opensea.io/assets")) {
-			let content: any =
-				document.getElementsByClassName("TradeStation--main")[0];
-			if (
-				content.getElementsByClassName("purchase-button-assets").length === 0
-			) {
-				const button: HTMLButtonElement = document.createElement("button");
-				button.className = "purchase-button-assets";
-				button.innerHTML = "Cop Now";
+    if (document.location.href.includes("https://opensea.io/assets")) {
+      let content: any =
+        document.getElementsByClassName("TradeStation--main")[0];
+      if (
+        content.getElementsByClassName("purchase-button-assets").length === 0
+      ) {
+        const button: HTMLButtonElement = document.createElement("button");
+        button.className = "purchase-button-assets";
+        button.innerHTML = "Cop Now";
 
-				button.onclick = async () => {
-					return event.emit("clickedAssets");
-				};
-				content.append(button);
-			}
-		}
-	}, 1000);
+        button.onclick = async () => {
+          assetId = document.location.href.split("/")[4];
+          tokenId = document.location.href.split("/")[5];
+          return event.emit("clickedAssets");
+        };
+        content.append(button);
+      }
+    }
+  }, 1000);
 }
 
 event.on("clickedActivity", async () => {
-	console.log("clicked");
+  let orderData: any = await utils.FetchOrderData(assetId, tokenId);
+  console.log(orderData?.orders[0]);
 
-	let orderData: any = await utils.FetchOrderData(assetId, tokenId);
-	console.log(orderData?.orders[0]);
+  if (orderData?.orders[0]?.side) {
+    await toast(true, "Initiating Quick Buy");
+    let params = {
+      assetId,
+      tokenId,
+    };
+    chrome.runtime.sendMessage({ params });
+  } else {
+    await toast(false, "Item is not listed for sale");
+  }
+});
 
-	await toast(true, "Initiating Quick Buy");
-	let params = {
-		assetId,
-		tokenId,
-	};
-	chrome.runtime.sendMessage({ params });
+event.on("clickedAssets", async () => {
+  let orderData: any = await utils.FetchOrderData(assetId, tokenId);
+  console.log(orderData);
 
-	// done
-	// await toast(false, "Error, this item isn't listed for sale");
+  if (orderData?.orders[0]?.side) {
+    await toast(true, "Initiating Quick Buy");
+    let params = {
+      assetId,
+      tokenId,
+    };
+    chrome.runtime.sendMessage({ params });
+  } else {
+    await toast(false, "Item is not listed for sale");
+  }
+});
 
-	// addrs first
-
-	// chrome.runtime.sendMessage({ params: params, myAddress: myAddress });
+chrome.runtime.onMessage.addListener(async (message, sender) => {
+  if (message?.error) {
+    await toast(message?.error?.error, message?.error?.message);
+  }
+  if (message?.success) {
+    await toast(message?.success?.success, message?.success?.message);
+  }
 });
 
 async function toast(status?: boolean, message?: string) {
-	if (status) {
-		const toastcontainer: HTMLDivElement = document.createElement("div");
-		const toast: HTMLDivElement = document.createElement("div");
-		const svg: SVGElement = document.createElementNS(
-			"http://www.w3.org/2000/svg",
-			"svg"
-		);
-		const path: SVGElement = document.createElementNS(
-			"http://www.w3.org/2000/svg",
-			"path"
-		);
-		svg.setAttribute("viewBox", "0 0 24 24");
-		svg.setAttribute("stroke", "#fff");
-		svg.setAttribute("fill", "#39cca5");
-		svg.setAttribute("class", "h-6 w-6");
-		svg.setAttribute("width", "24px");
-		svg.setAttribute("height", "24px");
-		path.setAttribute("stroke-linejoin", "round");
-		path.setAttribute("stroke-linecap", "round");
-		path.setAttribute("stroke-width", "2");
-		path.setAttribute("d", "M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z");
-		svg.appendChild(path);
-		toastcontainer.className = "toast-container";
-		toast.className = "toast";
-		toast.innerHTML = message || "";
-		toastcontainer.appendChild(svg);
-		toastcontainer.appendChild(toast);
-		document.body.append(toastcontainer);
+  if (status) {
+    const toastcontainer: HTMLDivElement = document.createElement("div");
+    const toast: HTMLDivElement = document.createElement("div");
+    const svg: SVGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    const path: SVGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("stroke", "#fff");
+    svg.setAttribute("fill", "#39cca5");
+    svg.setAttribute("class", "h-6 w-6");
+    svg.setAttribute("width", "24px");
+    svg.setAttribute("height", "24px");
+    path.setAttribute("stroke-linejoin", "round");
+    path.setAttribute("stroke-linecap", "round");
+    path.setAttribute("stroke-width", "2");
+    path.setAttribute("d", "M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z");
+    svg.appendChild(path);
+    toastcontainer.className = "toast-container";
+    toast.className = "toast";
+    toast.innerHTML = message || "";
+    toastcontainer.appendChild(svg);
+    toastcontainer.appendChild(toast);
+    document.body.append(toastcontainer);
 
-		setTimeout(() => {
-			document.body.removeChild(toastcontainer);
-		}, 5000);
-	} else {
-		const toastcontainer: HTMLDivElement = document.createElement("div");
-		const toast: HTMLDivElement = document.createElement("div");
-		const svg: SVGElement = document.createElementNS(
-			"http://www.w3.org/2000/svg",
-			"svg"
-		);
-		const path: SVGElement = document.createElementNS(
-			"http://www.w3.org/2000/svg",
-			"path"
-		);
-		svg.setAttribute("viewBox", "0 0 24 24");
-		svg.setAttribute("stroke", "#fff");
-		svg.setAttribute("fill", "#e02832");
-		svg.setAttribute("class", "h-6 w-6");
-		svg.setAttribute("width", "24px");
-		svg.setAttribute("height", "24px");
-		path.setAttribute("stroke-linejoin", "round");
-		path.setAttribute("stroke-linecap", "round");
-		path.setAttribute("stroke-width", "2");
-		path.setAttribute("d", "M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z");
-		svg.appendChild(path);
-		toastcontainer.className = "toast-container";
-		toast.className = "toast";
-		toast.innerHTML = message || "";
-		toastcontainer.appendChild(svg);
-		toastcontainer.appendChild(toast);
-		document.body.append(toastcontainer);
+    setTimeout(() => {
+      document.body.removeChild(toastcontainer);
+    }, 5000);
+  } else {
+    const toastcontainer: HTMLDivElement = document.createElement("div");
+    const toast: HTMLDivElement = document.createElement("div");
+    const svg: SVGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+    const path: SVGElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "path"
+    );
+    svg.setAttribute("viewBox", "0 0 24 24");
+    svg.setAttribute("stroke", "#fff");
+    svg.setAttribute("fill", "#e02832");
+    svg.setAttribute("class", "h-6 w-6");
+    svg.setAttribute("width", "24px");
+    svg.setAttribute("height", "24px");
+    path.setAttribute("stroke-linejoin", "round");
+    path.setAttribute("stroke-linecap", "round");
+    path.setAttribute("stroke-width", "2");
+    path.setAttribute("d", "M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z");
+    svg.appendChild(path);
+    toastcontainer.className = "toast-container";
+    toast.className = "toast";
+    toast.innerHTML = message || "";
+    toastcontainer.appendChild(svg);
+    toastcontainer.appendChild(toast);
+    document.body.append(toastcontainer);
 
-		setTimeout(() => {
-			document.body.removeChild(toastcontainer);
-		}, 5000);
-	}
+    setTimeout(() => {
+      document.body.removeChild(toastcontainer);
+    }, 5000);
+  }
 }
